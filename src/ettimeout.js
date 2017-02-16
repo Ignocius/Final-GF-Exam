04f455969badde572ed5753dc94a43719f835e0e
Szabo Rihcard <ignocius@gmail.com>
2017-02-16 21:42:28 +0100

    callback hel

--- src/ettimeout.js
+++ src/ettimeout.js
@@ -1,11 +1,34 @@
 function delay() {
   console.log('i am delayed :(');
 }
-function callMe(){
-  console.log('yooooo');
-}
+// function callMe(){
+//   console.log('yooooo');
+// }
+//
+// console.log('I am not delayed');
+// setTimeout(delay, 3000);
+//
+// setInterval(callMe, 3000);
 
-console.log('I am not delayed');
-setTimeout(delay, 3000);
-
-setInterval(callMe, 3000);
+(function() {
+  console.log(1);
+  (function() {
+    console.log(2);
+    (function() {
+      console.log(3);
+      (function() {
+        // console.log(4);
+        setTimeout(delay, 3000);
+        (function() {
+          console.log(5);
+          (function() {
+            console.log(6);
+            (function() {
+              console.log('last');
+            })();
+          })();
+        })();
+      })();
+    })();
+  })();
+})();
